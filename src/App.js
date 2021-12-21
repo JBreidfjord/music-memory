8eb38c2733ec0c2c241e73126495591d73311857
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-21 16:04:54 -0700

    Handle card matching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 
+import { useEffect, useState } from "react";
+
 import SingleCard from "./components/SingleCard";
-import { useState } from "react";
 
 const genNums = () => {
   // Generate 6 unique random integers between 1 and 358 (358 = number of images)
@@ -18,10 +19,12 @@ const genNums = () => {
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   const shuffleCards = () => {
     // Creates objects with filepath for generated image numbers
-    const cardImages = genNums().map((num) => ({ src: `/img/album/${num}.png` }));
+    const cardImages = genNums().map((num) => ({ src: `/img/album/${num}.png`, matched: false }));
 
     const shuffledCards = [...cardImages, ...cardImages] // Duplicate array creating pairs of cards
       .sort(() => 0.5 - Math.random()) // Randomly sort the array
@@ -31,13 +34,47 @@ function App() {
     setTurns(0);
   };
 
+  const handleChoice = (card) => {
+    choiceOne && choiceOne.id !== card.id ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 750);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
+
   return (
     <div className="App">
       <h1>Music Match</h1>
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard card={card} key={card.id} />
+          <SingleCard
+            card={card}
+            key={card.id}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
     </div>
